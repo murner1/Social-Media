f017407132019fc99d897c705ed379117b4f4f5c
murner1 <murner@mail.fresnostate.edu>
2022-05-24 07:35:34 -0700

    Update TweetRequestDto.java

--- src/main/java/com/cooksys/SocialMedia/Dtos/TweetRequestDto.java
+++ src/main/java/com/cooksys/SocialMedia/Dtos/TweetRequestDto.java
@@ -1,7 +1,15 @@
 package com.cooksys.SocialMedia.Dtos;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.cooksys.SocialMedia.Entities.Tweet;
+import com.cooksys.SocialMedia.Entities.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 
 @NoArgsConstructor
@@ -9,6 +17,20 @@ import lombok.NoArgsConstructor;
 
 public class TweetRequestDto {
     private int id;
+
+    private UserRequestDto author;
+
+    private Timestamp posted;
+
+    private String content;
+
+    private ContextDto inReplyTo;
+
+    private ContextDto repostOf;
+
+
+
+
 
 
 
