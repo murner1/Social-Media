b3ca43c95bc5014827c7c54f93fc01fccb4eb0d7
Michelle Randolph <danceofdisillusion@gmail.com>
2022-05-24 15:54:27 -0600

    Began ControllerAdvice

---
+++ src/main/java/com/cooksys/SocialMedia/Controllers/Advice/ControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.cooksys.SocialMedia.Controllers.Advice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+
+import com.fasterxml.jackson.databind.deser.impl.ErrorThrowingDeserializer;
+
+@RestControllerAdvice
+
+public class ControllerAdvice {
+	//BadRequest
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequest.class)
+	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+		return new ErrorDto(BadRequestException.getMessage());
+	}
+	
+	
+	//NotFoundException
+	
+	//NotAuthorizedException
+	
+	
+
+}
