17f17bbfecdd954e9140187eb3f30313c5035e88
murner1 <murner@mail.fresnostate.edu>
2022-05-23 17:46:44 -0700

    Create Credentials.java

--- src/main/java/com/cooksys/SocialMedia/Embeddable/Credentials.java
+++ src/main/java/com/cooksys/SocialMedia/Embeddable/Credentials.java
@@ -1,4 +1,9 @@
 package com.cooksys.SocialMedia.Embeddable;
+import javax.persistence.Embeddable;
 
+@Embeddable
 public class Credentials {
+    private String userName;
+
+    private String passWord;
 }
