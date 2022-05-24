d45a78690bee7760b6e25caee89c75ab49d200ee
murner1 <murner@mail.fresnostate.edu>
2022-05-23 19:08:36 -0700

    Create TweetRequestDto.java

---
+++ src/main/java/com/cooksys/SocialMedia/Dtos/TweetRequestDto.java
@@ -0,0 +1,17 @@
+package com.cooksys.SocialMedia.Dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Data
+
+public class TweetRequestDto {
+    private int id;
+
+    private
+
+
+
+}
