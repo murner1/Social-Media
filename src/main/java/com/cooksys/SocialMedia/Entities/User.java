ceb6e080488d61c8b52a1d654db019f0a217f75a
CoryJohnson101 <cory.johnson4545@gmail.com>
2022-05-23 22:00:07 -0400

    added the relationships for the hastag and user entities

--- src/main/java/com/cooksys/SocialMedia/Entities/User.java
+++ src/main/java/com/cooksys/SocialMedia/Entities/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -38,4 +39,33 @@ public class User {
 
     @Embedded
     private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = ("followers_following"),
+            joinColumns = @JoinColumn(name = "follower_id")
+    )
+    private List<User> followers;
+
+    @ManyToMany(mappedBy = "followers")
+    private List<User> following;
+
+    @ManyToMany
+    @JoinTable(
+            name = ("user_likes"),
+            joinColumns = @JoinColumn(name = "tweet_id")
+    )
+    private List<Tweet> likes;
+
+    @ManyToMany
+    @JoinTable(
+            name = ("user_mentions"),
+            joinColumns = @JoinColumn(name = "tweet_id")
+    )
+    private List<Tweet> mentions;
+
+    @OneToMany(mappedBy = "author")
+    @JoinColumn(name = "author")
+    private List<Tweet> tweets;
 }
+
