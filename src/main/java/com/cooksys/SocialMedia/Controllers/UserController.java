f20350bad459e27909af34b1d689839b497738d7
Michelle Randolph <danceofdisillusion@gmail.com>
2022-05-24 10:45:24 -0600

    Added UserController and updated requestMappin in TweetController

--- src/main/java/com/cooksys/SocialMedia/Controllers/UserController.java
+++ src/main/java/com/cooksys/SocialMedia/Controllers/UserController.java
@@ -3,15 +3,13 @@ package com.cooksys.SocialMedia.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.SocialMedia.Services.TweetService;
+import com.cooksys.SocialMedia.Services.UserService;
 
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("tweets")
-
+@RequestMapping("users")
 @AllArgsConstructor
-public class TweetController {
-	private TweetService tweetService;
-
+public class UserController {
+	private UserService userService;
 }
