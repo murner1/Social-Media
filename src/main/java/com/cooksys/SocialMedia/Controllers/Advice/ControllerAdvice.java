06bad7ef06fac66fa9f05e0afc556f7bfe105760
CoryJohnson101 <cory.johnson4545@gmail.com>
2022-05-24 23:05:40 -0400

    Created NotFoundException

--- src/main/java/com/cooksys/SocialMedia/Controllers/Advice/ControllerAdvice.java
+++ src/main/java/com/cooksys/SocialMedia/Controllers/Advice/ControllerAdvice.java
@@ -2,27 +2,37 @@ package com.cooksys.SocialMedia.Controllers.Advice;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.cooksys.SocialMedia.Dtos.ErrorDto;
+import com.cooksys.SocialMedia.Exceptions.BadRequestException;
+import com.cooksys.SocialMedia.Exceptions.NotFoundException;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import com.fasterxml.jackson.databind.deser.impl.ErrorThrowingDeserializer;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = {"com.cooksys.SocialMedia.Controllers"})
 
 public class ControllerAdvice {
 	//BadRequest
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(BadRequest.class)
 	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
-		return new ErrorDto(BadRequestException.getMessage());
+		return new ErrorDto(badRequestException.getMessage());
 	}
 	
 	
 	//NotFoundException
-	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException){
+		return new ErrorDto(notFoundException.getMessage());
+	}
 	//NotAuthorizedException
 	
 	
--- src/main/java/com/cooksys/SocialMedia/Exceptions/NotFoundException.java
+++ src/main/java/com/cooksys/SocialMedia/Exceptions/NotFoundException.java
@@ -1,4 +1,15 @@
 package com.cooksys.SocialMedia.Exceptions;
 
-public class NotFoundException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotFoundException extends RuntimeException  {
+
+    private String message;
+
+
 }
