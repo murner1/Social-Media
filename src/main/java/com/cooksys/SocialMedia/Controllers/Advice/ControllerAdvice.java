c2e43aca0b3280aeb80cadd4bf73073b53dac9d2
Michelle Randolph <danceofdisillusion@gmail.com>
2022-05-24 21:46:35 -0600

    Completed ControllerAdvice

--- src/main/java/com/cooksys/SocialMedia/Controllers/Advice/ControllerAdvice.java
+++ src/main/java/com/cooksys/SocialMedia/Controllers/Advice/ControllerAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
+import com.cooksys.SocialMedia.Exceptions.NotFoundException;
 import com.fasterxml.jackson.databind.deser.impl.ErrorThrowingDeserializer;
 
 @RestControllerAdvice
@@ -21,10 +22,22 @@ public class ControllerAdvice {
 	}
 	
 	
+	
+	
 	//NotFoundException
 	
-	//NotAuthorizedException
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+		return new ErrorDto(notFoundException.getMessage());
+	}
 	
+	//NotAuthorizedException
+	@ResponseStatus
+	@ExceptionHandler(NotAuthorizedException.class)
+	public ErrorDto handleNotAuthorizedException(HttpServletRequest request, NotAuthorizedException notAuthorizedException) {
+		return new ErrorDto(notAuthorizedException.getMessage());
+	}
 	
 
 }
