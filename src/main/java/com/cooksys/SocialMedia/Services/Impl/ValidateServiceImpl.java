acb2bae6b22890db7218108b01d003617ef4435f
murner1 <murner@mail.fresnostate.edu>
2022-05-25 13:00:01 -0700

    Implemented GET validate/username/available/@{username}
    Closes #45
    Implemented to GET for seeing if a username is available. Made changes to the ValidateService and ValidateController.

--- src/main/java/com/cooksys/SocialMedia/Services/Impl/ValidateServiceImpl.java
+++ src/main/java/com/cooksys/SocialMedia/Services/Impl/ValidateServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cooksys.SocialMedia.Services.Impl;
 
 import com.cooksys.SocialMedia.Entities.Hashtag;
+import com.cooksys.SocialMedia.Entities.User;
 import com.cooksys.SocialMedia.Repositories.HashtagRepository;
+import com.cooksys.SocialMedia.Repositories.UserRepository;
 import com.cooksys.SocialMedia.Services.ValidateService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,26 @@ import java.util.Optional;
 public class ValidateServiceImpl implements ValidateService {
 
 private final HashtagRepository hashtagRepository;
+
+private final UserRepository userRepository;
     @Override
     public boolean labelExists(String label) {
         Optional<Hashtag> optionalHashtag = hashtagRepository.findByLabel(label);
         return optionalHashtag.isPresent();
 
     }
+
+
+    @Override
+    public boolean usernameExists(String username){
+        Optional<User> optionalUsername = userRepository.findByLabel(username);
+        return optionalUsername.isPresent();
+    }
+
+    @Override
+    public boolean usernameAvailable(String username){
+        Optional<User> optionalUsername = userRepository.findByLabel(username);
+        return !optionalUsername.isPresent();
+    }
+
 }
