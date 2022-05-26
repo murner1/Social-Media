3323c74a3b064486433b68a3fa6729e9ddff0f4b
Michelle Randolph <danceofdisillusion@gmail.com>
2022-05-26 15:16:12 -0600

    Begain postTweet endpoint for a simple tweet

--- src/main/java/com/cooksys/SocialMedia/Controllers/TweetController.java
+++ src/main/java/com/cooksys/SocialMedia/Controllers/TweetController.java
@@ -1,8 +1,14 @@
 package com.cooksys.SocialMedia.Controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.SocialMedia.Dtos.TweetRequestDto;
+import com.cooksys.SocialMedia.Dtos.TweetResponseDto;
 import com.cooksys.SocialMedia.Services.TweetService;
 
 import lombok.AllArgsConstructor;
@@ -13,5 +19,17 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TweetController {
 	private TweetService tweetService;
+
+	// Post Tweets
+	// Creates a new simple tweet, with the author set to the user identified by the
+	// credentials in the request body. If the given credentials do not match an
+	// active user in the database, an error should be sent in lieu of a response.
+	// must have content cannot have inReplyTo or repostOf
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public TweetResponseDto postTweet(@RequestBody TweetRequestDto tweetRequestDto) {
+		return tweetService.postTweet(tweetRequestDto);
+	}
 
 }
--- src/main/java/com/cooksys/SocialMedia/Services/Impl/TweetServiceImpl.java
+++ src/main/java/com/cooksys/SocialMedia/Services/Impl/TweetServiceImpl.java
@@ -2,6 +2,12 @@ package com.cooksys.SocialMedia.Services.Impl;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.SocialMedia.Dtos.TweetRequestDto;
+import com.cooksys.SocialMedia.Dtos.TweetResponseDto;
+import com.cooksys.SocialMedia.Entities.Tweet;
+import com.cooksys.SocialMedia.Exceptions.BadRequestException;
+import com.cooksys.SocialMedia.Mappers.HashtagMapper;
+import com.cooksys.SocialMedia.Mappers.TweetMapper;
 import com.cooksys.SocialMedia.Repositories.TweetRepository;
 import com.cooksys.SocialMedia.Services.TweetService;
 
@@ -9,6 +15,29 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class TweetServiceImpl implements TweetService{
+public class TweetServiceImpl implements TweetService {
 	private TweetRepository tweetRepository;
+	private TweetMapper tweetMapper;
+
+	@Override
+	public TweetResponseDto postTweet(TweetRequestDto tweetRequestDto) {
+		// need to check that the credentials match an active user, if not send an error
+		// For now we don't need to check that it does not have inReplyTo or repostOf
+		// because tweetRequestDto only has content and credentials
+		
+		// Check that the tweet has content
+		if(tweetRequestDto.getContent()==null) {
+			throw new BadRequestException("Content is required for posting a tweet");
+		}
+		
+		Tweet tweetToSaveTweet = tweetMapper.requestDtoToEntity(tweetRequestDto);
+		// need to get the author of the tweet from the credentials
+		// need to scan through the tweeet for @{username} and #{hastag} and add these
+		// to the hashtags and usersMentioned lists
+		
+		
+		
+		// Save the tweet and and return it
+		return tweetMapper.entityToResponseDto(tweetRepository.saveAndFlush(tweetToSaveTweet));
+	}
 }
