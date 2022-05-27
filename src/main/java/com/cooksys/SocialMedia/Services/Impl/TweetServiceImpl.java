7f96935ecec30c0966ba3e81d42cb49958c20078
Michelle Randolph <danceofdisillusion@gmail.com>
2022-05-27 14:41:43 -0600

    Post tweet finds and adds hashtags

--- src/main/java/com/cooksys/SocialMedia/Services/Impl/TweetServiceImpl.java
+++ src/main/java/com/cooksys/SocialMedia/Services/Impl/TweetServiceImpl.java
@@ -1,19 +1,24 @@
 package com.cooksys.SocialMedia.Services.Impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.cooksys.SocialMedia.Dtos.TweetRequestDto;
 import com.cooksys.SocialMedia.Dtos.TweetResponseDto;
 import com.cooksys.SocialMedia.Dtos.UserResponseDto;
+import com.cooksys.SocialMedia.Entities.Hashtag;
 import com.cooksys.SocialMedia.Entities.Tweet;
 import com.cooksys.SocialMedia.Entities.User;
 import com.cooksys.SocialMedia.Exceptions.BadRequestException;
 import com.cooksys.SocialMedia.Exceptions.NotFoundException;
 import com.cooksys.SocialMedia.Mappers.TweetMapper;
 import com.cooksys.SocialMedia.Mappers.UserMapper;
+import com.cooksys.SocialMedia.Repositories.HashtagRepository;
 import com.cooksys.SocialMedia.Repositories.TweetRepository;
+import com.cooksys.SocialMedia.Repositories.UserRepository;
 import com.cooksys.SocialMedia.Services.TweetService;
+import com.cooksys.SocialMedia.Services.UserService;
 
 import org.springframework.stereotype.Service;
 
@@ -27,6 +32,10 @@ public class TweetServiceImpl implements TweetService {
     private TweetMapper tweetMapper;
 
     private UserMapper userMapper;
+    
+    private UserRepository userRepository;
+    
+    private HashtagRepository hashtagRepository;
 
     @Override
     public TweetResponseDto postTweet(TweetRequestDto tweetRequestDto) {
@@ -39,13 +48,37 @@ public class TweetServiceImpl implements TweetService {
             throw new BadRequestException("Content is required for posting a tweet");
         }
 
-        Tweet tweetToSaveTweet = tweetMapper.requestDtoToEntity(tweetRequestDto);
-        // need to get the author of the tweet from the credentials
+        
+     // get the author of the tweet from the credentials
+        Optional<User> user = userRepository.findByCredentialsUsername(tweetRequestDto.getCredentials().getUsername());
+        if (user.isEmpty() || user.get().isDeleted()) {
+        	throw new NotFoundException("Cannot create tweet. Cannot find user");
+        }
+        Tweet tweetToSave = tweetMapper.requestDtoToEntity(tweetRequestDto);
+
+        tweetToSave.setAuthor(user.get());
+        
         // need to scan through the tweeet for @{username} and #{hastag} and add these
+        String[] contentAsArray = tweetToSave.getContent().split(" ", 0);
+           
+        for (int i = 0; i <contentAsArray.length; i ++) {
+        	if(contentAsArray[i].contains("#")) {
+        		
+        		Hashtag hashtag = new Hashtag();
+        		hashtag.setLabel(contentAsArray[i]);
+        		hashtag.getTweets().add(tweetToSave);
+        		tweetToSave.getHashtags().add(hashtag);
+        		hashtagRepository.saveAndFlush(hashtag);
+        	}
+//        	if(contentAsArray[i].contains("@")) {
+//        		
+//        	}
+        }
+        
         // to the hashtags and usersMentioned lists
 
         // Save the tweet and and return it
-        return tweetMapper.entityToResponseDto(tweetRepository.saveAndFlush(tweetToSaveTweet));
+        return tweetMapper.entityToResponseDto(tweetRepository.saveAndFlush(tweetToSave));
     }
 
     @Override
